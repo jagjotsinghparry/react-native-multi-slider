f2936b56767907c07904f261646588af4e3f4b6e
Jagjot Singh <jagjot@qwibzio.com>
2019-01-31 14:25:00 +0530

    Customized slider

--- DefaultMarker.js
+++ DefaultMarker.js
@@ -24,39 +24,17 @@ class DefaultMarker extends React.Component {
 
 const styles = StyleSheet.create({
   markerStyle: {
-    ...Platform.select({
-      ios: {
-        height: 30,
-        width: 30,
-        borderRadius: 30,
-        borderWidth: 1,
-        borderColor: '#DDDDDD',
-        backgroundColor: '#FFFFFF',
-        shadowColor: '#000000',
-        shadowOffset: {
-          width: 0,
-          height: 3,
-        },
-        shadowRadius: 1,
-        shadowOpacity: 0.2,
-      },
-      android: {
-        height: 12,
-        width: 12,
-        borderRadius: 12,
-        backgroundColor: '#0D8675',
-      },
-    }),
+    height: 16,
+    width: 16,
+    borderRadius: 8,
+    borderWidth: 4,
+    borderColor: '#EBEBEB',
+    backgroundColor: '#AE2C23',
   },
   pressedMarkerStyle: {
-    ...Platform.select({
-      ios: {},
-      android: {
-        height: 20,
-        width: 20,
-        borderRadius: 20,
-      },
-    }),
+    height: 24,
+    width: 24,
+    borderRadius: 12,
   },
   disabled: {
     backgroundColor: '#d3d3d3',
